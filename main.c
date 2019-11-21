7970f52f21ace593dc62d703489719b5f106913b
miladiashe <55428028+miladiashe@users.noreply.github.com>
2019-11-21 15:20:39 +0900

    실습5

--- main.c
+++ main.c
@@ -5,9 +5,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[100] = "help me my code is not working";
+	int i;
+	FILE* txt;
+	char input[100];
 	
+	txt = fopen("sample.txt", "w"); 
+	for (i=0;i<3;i++)
+	{
+		printf("tell me.");
+		scanf("%s", input);
+		fprintf(txt, "%s\n", input);
+	}
 	
-	printf("문자열\"%s\" 의 길이 %i", str, strlen(str));
+	fclose(txt);
 	return 0;
 }
